d5744882607245cc4ea2ddd42a2d00a0d7716bb4
91637 <91637@LAPTOP-TP2EVIVT>
2021-12-09 15:04:18 +0530

    Commit&push2

--- src/test/java/SDET26githubTest/DemoTest.java
+++ src/test/java/SDET26githubTest/DemoTest.java
@@ -5,8 +5,15 @@ import org.testng.annotations.Test;
 public class DemoTest
 {   @Test
 	public void display()
-	{
-		
+	{ 
+	   System.out.println("Running display method");
+	  	
 	}
+    public void display2()
+    { 
+       System.out.println("Running display method2");
+  
+	
+     }
 
 }
